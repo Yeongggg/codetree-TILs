f40d721a5f65eb6bc9edd85bd06c98ac95dddf51
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-03-22 23:17:51 +0900

    Title: 씨 오 더블유 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240322/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+    int n; cin>>n;
+    string s; cin>>s;
+    int cnt =0;
+    for(int i =0; i<s.size(); i++){
+        if(s[i] == 'C'){
+             for(int j = i+1; j<s.size(); j++){
+                if(s[j] =='O'){
+                     for(int k = j+1; k<s.size(); k++){
+                        if(s[k] == 'W')cnt++;
+                    }
+                }
+           
+        }
+
+        }
+       
+    }
+    cout<<cnt;
+    return 0;
+}
