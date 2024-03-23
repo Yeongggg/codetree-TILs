8032e66fa1022ddc8a0cc821a2d8c059a47b36a3
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-03-24 03:32:19 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240324/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int count_digit(int num){
+    int cnt = 0;
+    while(num > 0){
+        num/=10;
+        cnt++;
+    }
+    return cnt;
+}
+
+bool check_carry(int a, int b){
+    bool result = true;
+    int a_digit = count_digit(a);
+    int b_digit = count_digit(b);
+
+    while(a_digit >0 || b_digit> 0){
+        int temp_a = a%10;
+        int temp_b = b%10;
+  
+        if(temp_a + temp_b >= 10){
+            result = false;
+            break;
+        }
+        a/=10;
+        b/=10;
+
+        a_digit--;
+        b_digit--;
+    }
+    
+    return result;
+}
+
+int main() {
+    int arr[21];
+
+    int n; cin>>n; 
+    for(int i =0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    int result =0;
+    int temp = 0;
+
+    for(int i =0; i<n; i++){
+        for(int j = i+1; j<n-1; j++){
+            if(check_carry(arr[i], arr[j])){
+                int temp_num = arr[i] + arr[j];
+                for(int k = j+1; k<n; k++){
+                    if(check_carry(temp_num, arr[k])){
+                        temp = temp_num + arr[k];
+                        result = max(result, temp);
+                        temp = 0;
+                    }
+
+                }
+            }
+            
+        }
+    }
+    cout<<result;
+    return 0;
+}
