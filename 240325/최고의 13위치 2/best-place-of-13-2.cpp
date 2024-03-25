98abd63095ecd96eca1cf12f3ec0cad6c1133262
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-03-25 17:15:54 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240325/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240325/최고의 13위치 2/best-place-of-13-2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<algorithm>
+#include<cstdlib>
 
 using namespace std;
 
@@ -13,23 +14,23 @@ int main() {
         }
     }  
     int ans = 0;
-    int temp = 0;
-    int temp_i = 0;
+   
     for(int i =0; i<n; i++){
-        
-        
-      for(int j=0; j<n-2; j++){
-        temp_i = arr[i][j] +arr[i][j+1]+ arr[i][j+2];
+        for(int j =0; j<n-2; j++){
+            for(int k=0; k<n; k++){
+                for(int l=0; l <n-2; l++){
+                    if(i == k && abs(j-2) <=2) continue;
 
-        for(int k = i+1; k<n; k++){
-            for(int l =0; l<n-2; l++){
-                 temp = arr[k][j] +arr[k][j+1]+ arr[k][j+2];
-                 ans = max(ans,temp + temp_i);
+                    int temp1 = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                    int temp2 = arr[k][l] + arr[k][l+1] + arr[k][l+2];
+
+                    ans = max(ans, temp1+ temp2);                
+                }
             }
-        }
-      }
-      
+        }   
     }
+      
+    
     cout<<ans;
     return 0;
 }
