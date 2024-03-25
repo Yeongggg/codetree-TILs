39ce523035200c379db32246a25f03de0221af17
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-03-25 17:42:09 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/오목/O-mok.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+
+using namespace std;
+
+int board[20][20];
+
+bool garo(int x, int y){
+    bool result = true;
+    int temp = board[x][y];
+
+    for(int i =y; i<y+4; i++){
+        if(i >= 18){
+            result = false; 
+            break;
+        }
+        if(board[x][i+1] != temp ){
+            result =false;
+            break;
+        }
+    }
+    return result;
+}
+
+bool sero(int x, int y){
+    bool result = true;
+ 
+    int temp = board[x][y];
+
+    for(int i =x; i<x+4; i++){
+        if(i >= 18){
+            result = false; 
+            break;
+        }
+        if(board[i][y] != temp ){
+            result =false;
+            break;
+        }
+    }
+    return result;
+    
+}
+
+bool cross(int x, int y){
+    bool result = true;
+    
+    for(int i =0; i<4; i++){
+        int dx = x+1;
+        int dy = y+1;
+        if(dx >18 || dy > 18){
+            result = false;
+            break;
+        }
+        if(board[x][y] != board[dx][dy]){
+            result = false;
+            break;
+        }
+        x +=1;
+        y+=1;
+    }
+    return result;
+}
+
+
+int main() {
+
+    for(int i =0; i<19; i++){
+        for(int j =0; j<19; j++){
+            cin >> board[i][j];
+        }
+    }
+     int white =0;
+    int black =0;
+    int x,y;
+
+    for(int i =0; i<19; i++){
+        for(int j =0; j<19; j++){
+
+           if(board[i][j] == 1){
+            bool one = garo(i,j);
+            bool two = sero(i,j);
+            bool three = cross(i,j);
+     
+            if(one || two || three){
+                black =1;
+                if(one){
+                    x= i;
+                    y = j + 2;
+                   
+                }else if(two){
+                    y = j;
+                    x = i+2;   
+    
+                }else{
+                    x = i+2;
+                    y = j+2;
+                  
+                }
+                break;
+            }
+           }else if(board[i][j] == 2){
+            bool one = garo(i,j);
+            bool two = sero(i,j);
+            bool three = cross(i,j);
+
+          
+            if(one || two || three){
+                white =1;
+          
+                  if(one){
+                    x= i;
+                    y = j + 2;
+                }else if(two){
+                    y = j;
+                    x = i+2;
+                   
+                }else{
+                    x = i+2;
+                    y = j+2;
+                }
+                break;
+            }
+           }
+        }
+    }
+
+
+
+   
+
+    if(black){
+        cout<<1<<'\n';
+
+    cout<<x+1<<' '<<y+1;
+    }else if(white){
+        cout<<2<<'\n';
+
+    cout<<x+1<<' '<<y+1;
+    }else{
+        cout<<0;
+    }
+    return 0;
+}
