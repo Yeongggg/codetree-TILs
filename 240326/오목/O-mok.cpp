0aebb43871ce5b5462fb014fd08847a93796885b
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-03-26 00:37:56 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/오목/O-mok.cpp
+++ 240326/오목/O-mok.cpp
@@ -5,31 +5,12 @@ int dx[] = {0,1,1,1,0,-1,-1,-1};
 int dy[] = {1,1,0,-1,-1,-1,0,1};
 //dx dy 테크닉 필요
 int board[19][19];
-int direction = 0;
 
-bool is_five(int x, int y){
-    bool result = false;
-    int cnt = 1;
-    for(int i =0; i<8; i++){
-        int nx  = x + dx[i];
-        int ny = y +dy[i];
-        if(nx < 0 || ny <0 || nx >= 19 || ny >= 19) continue;
-        for(int j =0; j<4; j++){
-            if(board[x][y] == board[nx][ny]) cnt++;
-            nx += dx[i];
-            ny += dy[i];
-            if(nx < 0 || ny <0 || nx >= 19 || ny >= 19) continue;
-        }
-        if(cnt == 5){
-            result = true;
-            direction = i;
-            return result;
-        }
-        cnt = 0;
-    }
-    return result;
 
+bool is_range(int x, int y){
+    return x >=0 && x < 19 && y >=0 && y <19;
 }
+
 int main() {
     for(int i =0; i<19; i++){
         for(int j =0; j<19; j++){
@@ -41,71 +22,28 @@ int main() {
     int x, y;
     for(int i =0; i<19; i++){
         for(int j =0; j<19; j++){
-            if(board[i][j] == 1){
-               if(is_five(i,j)){
-                cout<<1<<'\n';
-                if(direction == 0){
-                    cout<<i+1 << ' '<< j+3;
-                    return 0;
-                }else if(direction == 1){
-                    cout<<i+3<< ' '<< j+3;
-                    return 0;
-                }else if(direction == 2){
-                    cout<<i+3 << ' '<< j;
-                    return 0;
-                }else if(direction == 3){
-                    cout<<i+3 << ' '<< j-1;
-                    return 0;
-                }else if(direction == 4){
-                    cout<<i+1 << ' '<< j-1;
-                    return 0;
-                }else if(direction == 5){
-                    cout<<i-1 << ' '<< j-1;
-                    return 0;
-                }else if(direction == 6){
-                    cout<<i-1 << ' '<< j+1;
-                    return 0;
-                }else if(direction == 7){
-                    cout<<i-1 << ' '<< j+3;
-                    return 0;
+            int cur = board[i][j];
+            if(cur == 0)continue;
+
+            for(int k =0; k<8; k++){
+                bool isWin = true;
+                for(int l =0; l <5; l++){
+                    int nx = i +(dx[k] * l);
+                    int ny = j +(dy[k] * l);
+                    isWin = isWin && is_range(nx,ny)&&board[nx][ny] == cur;
                 }
-               }
-                          
-                }else if(board[i][j] == 2){
-                  
-                    if(is_five(i,j)){
-                cout<<2<<'\n';
-                if(direction == 0){
-                    cout<<i+1 << ' '<< j+3;
-                    return 0;
-                }else if(direction == 1){
-                    cout<<i+3<< ' '<< j+3;
-                    return 0;
-                }else if(direction == 2){
-                    cout<<i+3 << ' '<< j;
-                    return 0;
-                }else if(direction == 3){
-                    cout<<i+3 << ' '<< j-1;
-                    return 0;
-                }else if(direction == 4){
-                    cout<<i+1 << ' '<< j-1;
-                    return 0;
-                }else if(direction == 5){
-                    cout<<i-1 << ' '<< j-1;
-                    return 0;
-                }else if(direction == 6){
-                    cout<<i-1 << ' '<< j+1;
-                    return 0;
-                }else if(direction == 7){
-                    cout<<i-1 << ' '<< j+3;
-                    return 0;
+                if(isWin){
+                    cout<<cur<<'\n';
+                    cout<<i+1+dx[k]*2<<' '<<j+1+dy[k]*2;
+                return 0;
                 }
-                    }
-                }
+
+                
             }
         }
+    }
   
-
+    cout<<0;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
