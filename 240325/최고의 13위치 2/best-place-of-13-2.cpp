9750c22aa9d77a84651487bd99377844d0acf770
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-03-25 17:17:18 +0900

    Title: 최고의 13위치 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240325/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240325/최고의 13위치 2/best-place-of-13-2.cpp
@@ -19,7 +19,7 @@ int main() {
         for(int j =0; j<n-2; j++){
             for(int k=0; k<n; k++){
                 for(int l=0; l <n-2; l++){
-                    if(i == k && abs(j-2) <=2) continue;
+                    if(i == k && abs(j-l) <=2) continue;
 
                     int temp1 = arr[i][j] + arr[i][j+1] + arr[i][j+2];
                     int temp2 = arr[k][l] + arr[k][l+1] + arr[k][l+2];
