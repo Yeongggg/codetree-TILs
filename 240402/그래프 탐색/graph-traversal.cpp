64627acc1f5f3e602a94f69db1aa33911919d652
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-04-02 20:00:31 +0900

    Title: 그래프 탐색, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240402/그래프 탐색/graph-traversal.cpp
+++ 240402/그래프 탐색/graph-traversal.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int visited[1001];
 vector<int> v[1001];
-
+int cnt = 0;
 int n,m;
 
 void dfs(int node){
@@ -13,6 +13,7 @@ void dfs(int node){
     
     int cur = v[node][i];
     if(!visited[cur]){
+        cnt++;
         dfs(cur);
     }
 
@@ -24,18 +25,14 @@ int main() {
 
     for(int i =0; i<m; i++){
         int a,b; cin>> a>> b;
-        v[a-1].push_back(b-1);
-        v[b-1].push_back(a-1);
+        v[a].push_back(b);
+        v[b].push_back(a);
 
     }
-    int cnt =0;
 
-    for(int i =0; i<n; i++){
-      if(!visited[i]){
-        dfs(i);
-        cnt++;
-      }
-    }
+
+    dfs(1);
+
     cout<<cnt;
     return 0;
 }
