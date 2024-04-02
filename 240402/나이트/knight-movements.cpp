07a7dc4137c4f5d269facff73cb2f7a66818fb4b
Yeongggg <87257766+Yeongggg@users.noreply.github.com>
2024-04-02 21:54:11 +0900

    Title: 나이트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240402/나이트/knight-movements.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int r1,r2,c1,c2;
+
+int arr[101][101];
+int dis[101][101];
+int n;
+int dx[] ={-2,-1,1,2,2,1,-1,2};
+int dy[] ={1,2,2,1,-1,-2,-2,-1};
+
+void bfs(int x, int y){
+    dis[x][y] = 1;
+    queue<pair<int,int>> q;
+    q.push({x,y});
+    while(!q.empty()){
+        auto cur = q.front();
+        q.pop();
+
+        for(int i =0; i<8; i++){
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
+            if(nx <0 || ny <0 || nx >=n || ny >= n) continue;
+            if(dis[nx][ny] == 0){
+                q.push({nx,ny});
+                dis[nx][ny] = dis[cur.first][cur.second] + 1;
+            }
+        }
+    }
+   
+}
+
+
+int main() {
+
+    cin >> n;
+    cin>>r1>>r2>>c1>>c2;
+
+    bfs(r1-1,c1-1);
+    cout<<dis[r2-1][c2-1] -1;    
+    return 0;
+}
